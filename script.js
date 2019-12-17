516c0faddfd99ce721e279672ccb75aee116440c
B Legit <fungd@Donalds-MacBook-Air.local>
2019-12-16 19:39:04 -0500

    initial, final commit

--- script.js
+++ script.js
@@ -20,16 +20,71 @@ window.onload =  function(event) {
   // Do all of your work inside the window.onload function (in other words, here!)
 
   // Part 1
+  function a(){
+  let lessWordy= document.querySelector('#main-title')
+  lessWordy.innerHTML = 'DOM Toretto Homepage'
+}
+a()
+  // console.log(lessWordy)
 
   // Part 2
+  function b(){
+  let newColor= document.querySelector('body')
+  newColor.style.backgroundColor= 'blue'
+}
+b()
+  // console.log(newColor)
 
   // Part 3
-
+  function c(){
+  let removeLast = document.querySelectorAll('#favorite-things>li')
+  console.log(removeLast[5])
+  removeLast[removeLast.length-1].remove()
+}
+c()
+    // removeLast.parentNode.removeChild(ul[5])
   // Part 4
+  function d(){
+  let changeFontSz= document.querySelectorAll('.special-title')
+  console.log(changeFontSz)
 
+    for(i=0; i<changeFontSz.length; i++){
+       changeFontSz[i].style.fontSize= '2rem'
+    }
+  }
+  d()
+ 
   // Part 5
-
+function e(){
+  let removePastRace= document.querySelectorAll(`#past-races>li`)
+  console.log(removePastRace)
+  removePastRace[removePastRace.length-3].remove()
+}
+e()
   // Part 6
-
+function f(){
+  let addPastRace= document.querySelector(`#past-races`)
+  console.log(addPastRace)
+  let item= document.createElement('li')
+  item.innerHTML= "Miami"
+  addPastRace.append(item)
+}
+f()
   // Part 7
+function g(){
+  let newBlogPost = document.querySelector(`.main`)
+    console.log(newBlogPost)
+    let newBlog= document.createElement('div')
+    newBlog.classList.add("blog-post", "purple")
+    // newBlog.classList.add("p")
+    let city= document.createElement('h1')
+    let post= document.createElement('p')
+    city.innerHTML= "Miami"
+    post.innerHTML= "It was really hot."
+    newBlogPost.append(newBlog)
+    newBlog.append(city)
+    newBlog.append(post)
+}
+ g()
+    //create a div, createn h1 , add two classes to div., then append
 }
